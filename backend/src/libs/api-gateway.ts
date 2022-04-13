b21785d6e8ccb48e7c4af7a7663bbb5499b34504
Alex Krantz <alex@krantz.dev>
2022-04-12 19:39:47 -0700

    Attempt to fix cors rules (take 2)

--- backend/src/libs/api-gateway.ts
+++ backend/src/libs/api-gateway.ts
@@ -4,11 +4,22 @@ import type { FromSchema } from 'json-schema-to-ts';
 type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> };
 export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>;
 
-export const success = (status = 200) => ({ statusCode: status, body: '' });
+// CORS headers to return
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': true,
+};
+
+export const success = (status = 200) => ({ statusCode: status, body: '', headers });
 
 export const successWithJSON = (response: unknown, status = 200) => ({
   statusCode: status,
   body: JSON.stringify(response),
+  headers,
 });
 
-export const error = (status: number, reason: string) => ({ statusCode: status, body: JSON.stringify({ reason }) });
+export const error = (status: number, reason: string) => ({
+  statusCode: status,
+  body: JSON.stringify({ reason }),
+  headers,
+});
--- backend/src/libs/lambda.ts
+++ backend/src/libs/lambda.ts
@@ -6,7 +6,7 @@ export const middyfy = (handler) => middy(handler).use(middyJsonBodyParser());
 export const resolve = (context: string) => `${context.split(process.cwd())[1].substring(1).replace(/\\/g, '/')}`;
 
 export const cors = {
-  origin: 'https://id.wafflehacks.org',
+  origin: '*',
   headers: ['Content-Type', 'X-Api-Key', 'X-Amz-Security-Token', 'X-Amz-User-Agent', 'X-Amz-Date', 'Authorization'],
   allowCredentials: true,
 };
